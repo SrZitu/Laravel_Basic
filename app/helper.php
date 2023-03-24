e0311bde9989c78a8f4429f0c1126307eb83cd9b
SrZitu <zitu094@gmail.com>
2023-03-24 20:16:31 +0600

    helpers.php

---
+++ app/helper.php
@@ -0,0 +1,17 @@
+<?php
+//important function
+if(!function_exists('p')){
+    function p($data){
+        echo "<pre>";
+        print_r($data);
+        echo "<pre>";
+    }
+}
+
+//whenerver there is need to format a date i will just call this function from there
+if(!function_exists('get_formatted_date')){
+    function get_formatted_date($date,$format){
+$formattedDate=date($format,strtotime($date));
+return $formattedDate;
+    }
+}
